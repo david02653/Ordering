1a005df45a07216b8c273c1de6e1893355f49cb3
Chen <i11007020@gmail.com>
2019-01-23 05:53:58 +0800

    commit

--- src/main/java/Ordering/Ordering.java
+++ src/main/java/Ordering/Ordering.java
@@ -60,27 +60,22 @@ public class Ordering {
 	         System.out.println("MongoDBConnect to database successfully");
 	         
 	         MongoCollection<Document> collection = mongoDatabase.getCollection("orderingList");
-	         /*
-	         Document doc1 = new Document("name", "Amarcord Pizzeria")
-	        		    .append("contact", new Document("phone", "264-555-0193")
-	        		    .append("email", "amarcord.pizzeria@example.net")
-	        		    .append("location",Arrays.asList(-73.88502, 40.749556)))
-	        		    .append("stars", 2)
-	        		    .append("categories", Arrays.asList("Pizzeria", "Italian", "Pasta"));
+	         
+	         
+	         Document doc1 = new Document("ObjectID", "123")
+	        		    .append("Category", "test")
+	        		    .append("Quantity", 666);
 	
-	        		Document doc2 = new Document("name", "Blue Coffee Bar")
-	        		    .append("contact", new Document("phone", "604-555-0102")
-	        		    .append("email", "bluecoffeebar@example.com")
-	        		    .append("location",Arrays.asList(-73.97902, 40.8479556)))
-	        		    .append("stars", 5)
-	        		    .append("categories", Arrays.asList("Coffee", "Pastries"));
-	
-	        		ArrayList<Document> documents = new ArrayList<Document>();
-	        		documents.add(doc1);
-	        		documents.add(doc2);
-	
-	        		collection.insertMany(documents);
-			*/
+	         Document doc2 = new Document("ObjectID", "321")
+	        		    .append("Category", "test2")
+	        		    .append("Quantity", 222);
+
+    		ArrayList<Document> documents = new ArrayList<Document>();
+    		documents.add(doc1);
+    		documents.add(doc2);
+
+    		collection.insertMany(documents);
+			
 	         return "success";
 		} catch (Exception e) {  
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );

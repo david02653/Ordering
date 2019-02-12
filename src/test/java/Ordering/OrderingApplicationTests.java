6247e5e78047ba19883a24701d1c4c44bb6fac2e
Chen <i11007020@gmail.com>
2019-02-13 06:35:37 +0800

    commit

--- src/test/java/Ordering/OrderingApplicationTests.java
+++ src/test/java/Ordering/OrderingApplicationTests.java
@@ -4,18 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-import Ordering.*;
+//Ctrl+Shift+O 可以自動導入需要的包
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = OrderingApplication.class)
+@WebAppConfiguration
 public class OrderingApplicationTests {
 
 	@Autowired
 	Ordering ordering;
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception{
 				
 		ordering.notification("1", "Ordering successfully");
 		

2d83b90053dc3594461c6c580d70283393c4a5bd
i11007020 <i11007020@gmail.com>
2019-02-13 11:30:57 +0800

    commit

--- src/main/java/Ordering/SwaggerConfig.java
+++ src/main/java/Ordering/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("CinemaCatalog"))
+                .apis(RequestHandlerSelectors.basePackage("Ordering"))
                 .paths(PathSelectors.any())
                 .build();
     }

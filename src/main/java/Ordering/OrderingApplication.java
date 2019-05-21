a4acbf0c80392b42bca4a07c01ccff398b0d1854
i11007020 <i11007020@gmail.com>
2019-05-21 12:47:14 +0800

    update Eureka Zuul

--- src/main/java/Ordering/OrderingApplication.java
+++ src/main/java/Ordering/OrderingApplication.java
@@ -2,10 +2,12 @@ package Ordering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaServer
+
+
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderingApplication {
 
 	public static void main(String[] args) {

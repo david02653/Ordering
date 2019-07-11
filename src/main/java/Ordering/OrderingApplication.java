aa6a81d5ac081d687e6034fd1034ee18048e9c99
Chen <i11007020@gmail.com>
2019-07-11 22:49:26 +0800

    commit

--- src/main/java/Ordering/OrderingApplication.java
+++ src/main/java/Ordering/OrderingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
-
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients

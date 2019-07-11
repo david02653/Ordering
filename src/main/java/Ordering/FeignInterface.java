bf44e6eb555930b08c861048194e49bb319e1acc
Chen <i11007020@gmail.com>
2019-07-11 21:43:50 +0800

    update

--- src/main/java/Ordering/FeignInterface.java
+++ src/main/java/Ordering/FeignInterface.java
@@ -1,21 +1,24 @@
 package Ordering;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="api-gateway")
 public interface FeignInterface {
-	/*
+
 	@RequestMapping(value = "/notification/newNotification", method = RequestMethod.GET)
     public String notification(@RequestParam("userID") String userID, @RequestParam("content") String content);
 	
 	@RequestMapping(value = "/payment", method = RequestMethod.GET)
     public String payment();
-	*/
+
+	/*
     @GetMapping("/notification/newNotification")
     String notification(@PathVariable("userID") String userID, @PathVariable("content") String content);
 
     @GetMapping("/payment")
     String payment();
+	 */
 }

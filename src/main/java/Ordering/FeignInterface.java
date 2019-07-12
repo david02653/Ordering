015f212c440153a6e4420cf1772aa05321f53b20
Chen <i11007020@gmail.com>
2019-07-12 15:42:48 +0800

    git push

--- src/main/java/Ordering/FeignInterface.java
+++ src/main/java/Ordering/FeignInterface.java
@@ -3,16 +3,18 @@ package Ordering;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="api-gateway")
+@FeignClient(name="payment")
 public interface FeignInterface {
 
-	@RequestMapping(value = "/notification/newNotification", method = RequestMethod.GET)
-    public String notification(@RequestParam("userID") String userID, @RequestParam("content") String content);
+//	@RequestMapping(value = "/notification/newNotification", method = RequestMethod.GET)
+//    public String notification(@RequestParam("userID") String userID, @RequestParam("content") String content);
 
-    @RequestMapping(value = "/notification/getNotification", method = RequestMethod.GET)
-    public String payment(@RequestParam("userID") String userID);
+	@RequestMapping(value = "/payment", method = RequestMethod.GET)
+    public String payment();
+
+
+
 
 	/*
     @GetMapping("/notification/newNotification")
--- src/main/java/Ordering/OrderingController.java
+++ src/main/java/Ordering/OrderingController.java
@@ -99,7 +99,7 @@ public class OrderingController {
 		String result = "";
 		try {
 			
-			result = feignInterface.payment(userID);
+			result = feignInterface.payment();
 			
 			
 		} catch (Exception e) {

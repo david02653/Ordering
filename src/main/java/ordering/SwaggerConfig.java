9f33d73d5fbf4b1cffa5b8378a362d7b1a687019
Chen <i11007020@gmail.com>
2019-07-15 15:22:11 +0800

    Refactor

--- src/main/java/ordering/SwaggerConfig.java
+++ src/main/java/ordering/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("Ordering"))
+                .apis(RequestHandlerSelectors.basePackage("ordering"))
                 .paths(PathSelectors.any())
                 .build();
     }

fe62a04d637581cad4951fb8d2fe4510b8b4c1a8
i11007020 <i11007020@gmail.com>
2020-01-02 06:20:36 +0800

    commit

---
+++ target/generated-test-sources/contracts/ordering/ContractVerifierTest.java
@@ -0,0 +1,33 @@
+package ordering;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
+import io.restassured.response.ResponseOptions;
+import ordering.ProviderRoleBaseTestClass;
+import org.junit.Test;
+
+import static com.toomuchcoding.jsonassert.JsonAssertion.assertThatJson;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static org.springframework.cloud.contract.verifier.assertion.SpringCloudContractAssertions.assertThat;
+
+public class ContractVerifierTest extends ProviderRoleBaseTestClass {
+
+	@Test
+	public void validate_shouldReturnRequest() throws Exception {
+		// given:
+			MockMvcRequestSpecification request = given();
+
+		// when:
+			ResponseOptions response = given().spec(request)
+					.queryParam("number","2")
+					.get("/validate/prime-number");
+
+		// then:
+			assertThat(response.statusCode()).isEqualTo(200);
+		// and:
+			String responseBody = response.getBody().asString();
+			assertThat(responseBody).isEqualTo("Even");
+	}
+
+}

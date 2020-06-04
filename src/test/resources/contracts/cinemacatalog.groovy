6a843b4659991cb334f2bf170e9ddd2389a93daf
i11007020 <i11007020@gmail.com>
2020-06-04 20:10:59 +0800

    commit

--- src/test/resources/contracts/cinemacatalog.groovy
+++ src/test/resources/contracts/cinemacatalog.groovy
@@ -26,7 +26,7 @@ import org.springframework.cloud.contract.spec.Contract
                 method ("GET")
                 url("/newMovieOrdering") {
                     queryParameters {
-                        parameter("userID", 1)
+                        parameter("userID", "1")
                         parameter("moviesID", "5e0b10fa974ef74883b43403")
                     }
                 }
